081a993a9ac702cdf95135c07fd733f1629d7657
QuocBao <baolmq05@gmail.com>
2026-08-29 19:30:06 +0700

    fix: trust railway proxy and force https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         Authenticate::redirectUsing(fn () => null);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
